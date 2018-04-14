0f90733109eab19fbbca701d48dc2ef2ad097fa6
lxhdj0215 <wangguijun.123@163.com>
2018-04-14 14:19:26 +0800

    ssm

---
+++ dubbo-consumer/src/main/java/com/alibaba/dubbo/consumer/Consumer.java
@@ -0,0 +1,29 @@
+package com.alibaba.dubbo.consumer;
+
+
+import java.io.IOException;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.lxhdj.dubbo.service.DubboService;
+
+public class Consumer {
+	public static void main(String[] args) throws IOException, InterruptedException {
+		// 测试常规服务
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("consumer.xml");
+		context.start();
+		System.out.println("consumer start");
+		while (true) {
+			try {
+				DubboService dubboService = context.getBean(DubboService.class);
+				System.out.println(dubboService.getName("world"));
+				dubboService.sayHello();
+				Thread.sleep(1000 * 5);
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+				Thread.sleep(1000 * 5);
+			}
+
+		}
+	}
+}
---
+++ web/src/main/java/com/lxhdj/mybatis/inter/IUserOperation.java
@@ -0,0 +1,21 @@
+package com.lxhdj.mybatis.inter;
+
+import java.util.List;
+
+import com.lxhdj.mybatis.model.Article;
+import com.lxhdj.mybatis.model.User;
+
+public interface IUserOperation {
+	public User selectUserByID(int id);
+
+	public List<User> selectUsers(String userName);
+
+	public void addUser(User user);
+
+	public void updateUser(User user);
+
+	public boolean deleteUser(int id);
+
+	public List<Article> getUserArticles(int id);
+
+}
